58ab1a8dc90a809b92b16a02c8edcf75281ca286
Charlesarin101 <arinonebusiness@gmail.com>
2026-02-19 06:02:04 +0100

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,10 +1,8 @@
 // supabase.js
+// Global client: window.supabaseClient
+
 const SUPABASE_URL = "https://abingfaxdrtxceriulxt.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFiaW5nZmF4ZHJ0eGNlcml1bHh0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEyNTkzNTQsImV4cCI6MjA4NjgzNTM1NH0.PydZNseDl05riGoAXwOThe95ZuYdamdJ1vriiN9FIk0";
-
-if(!SUPABASE_URL || !SUPABASE_ANON_KEY){
-  console.error("Supabase config missing.");
-}
 
 window.supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
